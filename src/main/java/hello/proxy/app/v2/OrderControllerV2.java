d818aef900bd308f2af012f71a823ec72f3cea53
iokim <iokim1220@gmail.com>
2022-03-18 12:45:40 +0900

    # 예제 프로젝트 만들기 v2

---
+++ src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,29 @@
+package hello.proxy.app.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@RequestMapping // @Controller 가 아닌 @RequestMapping을 쓰는 이유는 컴포넌트 스캔을 통해서가 아니라 수동으로 빈을 등록하기 위해서
+@ResponseBody
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return null;
+    }
+}
