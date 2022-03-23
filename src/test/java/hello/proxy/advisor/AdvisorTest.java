e6dcaf4e426f3113c64977bb47c458f25b3ef5d2
iokim <iokim1220@gmail.com>
2022-03-23 14:41:36 +0900

    # 예제 코드 2 - 직접 만든 포인트컷

--- src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -3,11 +3,18 @@ package hello.proxy.advisor;
 import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 
+import java.lang.reflect.Method;
+
+@Slf4j
 public class AdvisorTest {
 
     @Test
@@ -20,5 +27,55 @@ public class AdvisorTest {
 
         proxy.save();
         proxy.find();
+    }
+
+    @Test
+    @DisplayName("직접 만든 포인트컷")
+    void advisorTest2() {
+        ServiceImpl target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointCut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    static class MyPointCut implements Pointcut {
+
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    static class MyMethodMatcher implements MethodMatcher {
+
+        private String matchName = "save";
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            boolean result = method.getName().equals(matchName);
+            log.info("포인트컷 호출 method.getName(), targetClass={}", method.getName(), targetClass);
+            log.info("포인트컷 결과 result={}", result);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false; // false 인 경우 클래스 정적 정보 사용 & 캐싱 사용. true 인 경우 매개변수가 동적으로 변경된다고 가정하기 때문에 캐싱을 하지 않는다...
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            return false;
+        }
     }
 }
