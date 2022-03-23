708c0b5cdc6b974beac2b3e6463fdf069bb12ae8
iokim <iokim1220@gmail.com>
2022-03-23 11:57:38 +0900

    # 프록시 팩토리 - 예제 코드 2

--- src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -1,6 +1,7 @@
 package hello.proxy.proxyfactory;
 
 import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ConcreteService;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +21,8 @@ public class ProxyFactoryTest {
     void interfaceProxy() {
         ServiceInterface target = new ServiceImpl();
         ProxyFactory proxyFactory = new ProxyFactory(target); // 프록시팩토리 생성시 target 넘겨줌
+//        proxyFactory.setProxyTargetClass(true); // 중요! 인터페이스가 있어도 항상 CGLIB, 구체클래스 기반으로 만들어짐
+
         proxyFactory.addAdvice(new TimeAdvice());
         ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
         log.info("target.getClass()={}", target.getClass());
@@ -31,4 +34,22 @@ public class ProxyFactoryTest {
         assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
         assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
     }
+
+    @Test
+    @DisplayName("구체 클래스만 있으면 CGLIB 사용")
+    void concreteProxy() {
+        ConcreteService target = new ConcreteService();
+        ProxyFactory proxyFactory = new ProxyFactory(target); // 프록시팩토리 생성시 target 넘겨줌
+        proxyFactory.addAdvice(new TimeAdvice());
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+        log.info("target.getClass()={}", target.getClass());
+        log.info("proxy.getClass()={}", proxy.getClass());
+
+        proxy.call();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue(); // ProxyFactory를 쓸때만 사용해볼수 있음
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
 }
